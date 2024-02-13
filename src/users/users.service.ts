880447564d3ff6f31c998430fe818ccd019e6852
venilson2 <venilson2009@hotmail.com>
2024-02-13 20:31:54 -0300

    first commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+    private users: any[];
+    constructor() {
+        this.users = [
+          {
+            userId: 1,
+            username: 'john',
+            password: 'changeme',
+          },
+          {
+            userId: 2,
+            username: 'maria',
+            password: 'guess',
+          },
+        ];
+    }
+
+    
+  async findOne(username) {
+    return this.users.find(user => user.username === username);
+  }
+}
