a44bae4016fbfdb23d17f09d172421d6d856e5ce
venilson2 <venilson2009@hotmail.com>
2024-03-17 19:13:56 -0300

    feat(exception): tratamento de erros global

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get, Param, Put, Delete, NotFoundException, Res, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Put, Delete, NotFoundException, Res, HttpStatus, ConflictException } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
@@ -12,65 +12,57 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  async findAll(@Res() res: Response): Promise<User[] | void> {
+  async findAll(): Promise<User[] | void> {
     try{
       const users = await this.usersService.findAll();
-      res.status(HttpStatus.OK).json(users)
+      return users
     } catch (error){
-      res.json(error).status(error.status);
+      return error
     }
   }
 
   @Get(':id')
-  async findById(@Param('id') id: string, @Res() res: Response): Promise<User | void> {
-    try {
-      const user = await this.usersService.findById(id);
-      if (!user) throw new NotFoundException('User not found');
-      res.status(HttpStatus.OK).json(user)
-    } catch (error) {
-      res.json(error).status(error.status);
+  async findById(@Param('id') id: string): Promise<User> {
+    const user = await this.usersService.findById(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
     }
+    return user;
   }
 
   @Post()
-  async create(@Body() createUserDto: CreateUserDto, @Res() res: Response): Promise<User | void> {
-    try {
-      const { username, phone_number, email } = createUserDto;
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    const { username, phone_number, email } = createUserDto;
 
-      const existingUserByUsername = await this.usersService.findByUsername(username);
-      if (existingUserByUsername) res.status(HttpStatus.CONFLICT).json({meesage: 'Conflict: Username already exists'});
+    const existingUserByUsername = await this.usersService.findByUsername(username);
+    if (existingUserByUsername)  throw new ConflictException('Username already exists');
 
-      const existingUserByEmail = await this.usersService.findByEmail(email);
-      if (existingUserByEmail) res.status(HttpStatus.CONFLICT).json({message: 'Conflict: E-mail already exists'});
+    const existingUserByEmail = await this.usersService.findByEmail(email);
+    if (existingUserByEmail) throw new ConflictException('E-mail already exists');
 
-      const existingUserByPhoneNumber = await this.usersService.findByPhoneNumber(phone_number);
-      if (existingUserByPhoneNumber) res.status(HttpStatus.CONFLICT).json({message: 'Conflict: Phone number already exists'});
+    const existingUserByPhoneNumber = await this.usersService.findByPhoneNumber(phone_number);
+    if (existingUserByPhoneNumber) throw new ConflictException('Phone number already exists');
 
-      const user = await this.usersService.create(createUserDto);
-      res.json(user).status(HttpStatus.CREATED);
-    } catch (error) {
-      res.json(error).status(error.status);
-    }
+    return await this.usersService.create(createUserDto);
   }
 
   @Put(':id')
   async update(
     @Param('id') id: string,
-    @Body() updateUserDto: UpdateUserDto,
-    @Res() res: Response
+    @Body() updateUserDto: UpdateUserDto
   ): Promise<User | void> {
     try {
       const user = await this.usersService.update(id, updateUserDto);
       if (!user) throw new NotFoundException('User not found');
-      res.json(user).status(HttpStatus.OK)
+      return user
     } catch (error) {
-      res.json(error).status(error.status);
+      return error
     }
   }
 
   @Delete(':id')
   async remove(
-    @Param('id') id: string, 
+    @Param('id') id: string,
   ) {
     try {
       const user = await this.usersService.remove(id);
